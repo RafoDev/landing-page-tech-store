77e22896c6a8bf0aa7f4473cbe18948520a36c82
mariela-rodriguez <mariela.rodriguez@pucp.pe>
2022-05-01 20:15:58 -0500

    header dark mode button

---
+++ scripts/darkMode.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// const documentReady = () => {
+  const btnSwitch = document.querySelector('#header-nav__menu-switch');
+
+  console.log(btnSwitch);
+
+  btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('body-dark');
+    // btnSwitch.classList.toggle('active');
+  });
+  //   // Guardamos el modo en localstorage.
+  //   if(document.body.classList.contains('dark')){
+  //     localStorage.setItem('dark-mode', 'true');
+  //   } else {
+  //     localStorage.setItem('dark-mode', 'false');
+  //   }
+  // });
+
+  // // Obtenemos el modo actual.
+  // if(localStorage.getItem('dark-mode') === 'true'){
+  //   document.body.classList.add('dark');
+  //   btnSwitch.classList.add('active');
+  // } else {
+  //   document.body.classList.remove('dark');
+  //   btnSwitch.classList.remove('active');
+  // }
+// }
